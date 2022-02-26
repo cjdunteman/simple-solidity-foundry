e613ffca82a7848a3f2cad6d1589345ec8b1a847
CJ Dunteman <cjdunteman@gmail.com>
2022-02-26 02:21:33 -0600

    start faucet

--- src/Faucet.sol
+++ src/Faucet.sol
@@ -5,7 +5,10 @@ contract Faucet {
 
     receive () external payable {}
 
-    function drip () public {
-        msg.sender.transfer(1000);
+    function drip (uint256 amount) public {
+        require((msg.sender).balance < 10000000000, "you have enough already");
+        require((address(this)).balance >= amount, "the faucet does not have enough");
+
+        payable(address(msg.sender)).transfer(amount);
     }
 }
--- src/test/Faucet.t.sol
+++ src/test/Faucet.t.sol
@@ -4,12 +4,25 @@ pragma solidity 0.8.10;
 import "ds-test/test.sol";
 import "src/Faucet.sol";
 
+interface CheatCodes {
+    function prank(address) external; 
+    function expectRevert(bytes calldata) external;
+}
+
 contract FaucetTest is DSTest {
+    CheatCodes cheats = CheatCodes(HEVM_ADDRESS);
     Faucet faucet;
 
-    function setUp() public {}
+    function setUp() public {
+        faucet = new Faucet();
+    }
 
-    function testExample() public {
-        assertTrue(true);
+    function testDripFaucetBalTooLow () public {
+        payable(address(faucet)).transfer(1001);
+        cheats.prank(address(0));
+        faucet.drip(1000);
+        cheats.expectRevert(
+            bytes("the faucet does not have enough")
+        );
     }
 }
